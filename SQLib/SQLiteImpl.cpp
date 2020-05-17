a4e37403a847fd730859deea21756964cfe21f86
feltos <guillaumevolgyesi@gmail.com>
2020-05-17 22:37:00 +0200

    rendu

--- SQLib/SQLiteImpl.cpp
+++ SQLib/SQLiteImpl.cpp
@@ -9,29 +9,52 @@ namespace sql {
 
 	static int Callback(void* ptr, int ac, char** av, char** column_name) 
 	{
-#pragma message ("You have to complete this code!")
+		auto* sqlite_impl = static_cast<SQLiteImpl*>(ptr);
+
 		return 0;
 	}
 
+	//DONE
 	SQLiteImpl::SQLiteImpl(const std::string& file)
 	{
-#pragma message ("You have to complete this code!")
+		auto sqlOpen = sqlite3_open(file.c_str(),&db_);
+
+		if (sqlOpen != 0) {
+			throw std::runtime_error("SqlOpen error");
+		}
 	}
 
+	//DONE
 	SQLiteImpl::~SQLiteImpl()
 	{
-#pragma message ("You have to complete this code!")
+		sqlite3_close(db_);
 	}
 
 	bool SQLiteImpl::ExecuteString(const std::string& cmd)
 	{
-#pragma message ("You have to complete this code!")
+		error_.clear();
+		table_.clear();
+
+		auto test = &error_;
+
+		//can't reach adress of error
+		//sqlite3_exec(db_,cmd.c_str(),Callback,this,error pointers);
 		return true;
 	}
 
 	sql::value_t SQLiteImpl::GetValue(const std::string& str) const
 	{
-#pragma message ("You have to complete this code!")
+		if (str.empty()) { return nullptr;}
+
+		//check for double
+		int nb_points = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str[i] == '.') { nb_points++; }
+		}
+		if (nb_points <= 1) {
+			return str;
+		}
+
 		return str;
 	}
 
